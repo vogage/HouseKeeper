45fe9b8621b307ab9bb683646bb951e25cf504d6
vogage <1309416226@qq.com>
2024-04-19 11:13:04 +0800

    add webrtc-andriod files

---
+++ AppClient/Android/webrtc-in-jetpack-compose-main/webrtc-android/settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+        maven { url=uri ("https://maven.aliyun.com/repository/releases")}
+        maven { url=uri ("https://maven.aliyun.com/repository/google")}
+        maven { url=uri ("https://maven.aliyun.com/repository/central")}
+        maven { url=uri ("https://maven.aliyun.com/repository/gradle-plugin")}
+        maven { url=uri ("https://maven.aliyun.com/repository/public")}
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+        maven(url = "https://plugins.gradle.org/m2/")
+        maven { url=uri ("https://maven.aliyun.com/repository/releases")}
+        maven { url=uri ("https://maven.aliyun.com/repository/google")}
+        maven { url=uri ("https://maven.aliyun.com/repository/central")}
+        maven { url=uri ("https://maven.aliyun.com/repository/gradle-plugin")}
+        maven { url=uri ("https://maven.aliyun.com/repository/public")}
+    }
+}
+rootProject.name = "webrtc-sample-compose"
+include(":app")
